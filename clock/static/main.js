3c3c3a8b671c34b6ce6e2ad6e540a7ed6a1cf02c
Human-Ai-Course <okumura.yu@p.chibakoudai.jp>
2026-05-12 18:52:56 +0900

    Delete clock directory

--- clock/static/main.js
+++
@@ -1,9 +0,0 @@
-function clockReceive(data) {
-  document.getElementById("date").innerText = data;
-}
-
-// WebSocketの通信準備
-const ws = io.connect();
-
-// 時刻を受信したときに呼び出す関数を決める
-ws.on("message", clockReceive);
